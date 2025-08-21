0a8fff0bcf3f66beb729049eb7b95720260cbe33
Pedro_Alves <132352503+Pedroza1308@users.noreply.github.com>
2025-08-21 22:24:28 +0000

    teste-2

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -1,15 +1,27 @@
-export default function Home(){
-  return(
-    <div> 
-      <img src="/assets/foto-minha.jpg" height={300} width={300}></img>
-      <h1>Olá, me chamo Pedro Alves</h1>
-      <hr></hr>
-      <p>Teste1</p>
-      <hr></hr>
-      <p>Sou apaixonado por música e pra compartilhar um pouco mais de mim vou mostrar uma canção que estive escutando bastante recentemente: </p>
-      <audio src="/assets/a-primeira-vista.mp4" controls></audio>
-    </div>
+export default function Home() {
+  return (
+    <div>
+      <img src="/assets/foto-minha.jpg" height={300} width={300} />
 
-  )
-    
+      <h1>Olá, me chamo Pedro Alves</h1>
+
+      <hr />
+
+      <p>
+        Tenho 21 anos e sou estudante do 5&ordm;/6&ordm; período do curso Ciência da Computação na Universidade Católica de Pernambuco.
+        Gosto muito de aprender e conhecer novas pessoas. Duas curiosidades sobre minha pessoa são:
+        Primeiro, antes de ingressar na Católica, fiz dois períodos de Matemática Bacharelado na Universidade Federal de Pernambuco,
+        porém não acabei me identificando muito com o curso. Segundo: já fui campeão pernambucano de futsal sub-11 pela escolinha HS Society.
+        Sou o caçula de três irmãos e amo muito minha família.
+      </p>
+
+      <hr />
+
+      <p>
+        Sou apaixonado por música e, pra compartilhar um pouco mais de mim, vou mostrar uma canção que estive escutando bastante recentemente:
+      </p>
+
+      <audio src="/assets/a-primeira-vista.mp4" controls />
+    </div>
+  );
 }
