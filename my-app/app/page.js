37d9c700b31b0914f542a3889cc49d46571e9001
Pedro_Alves <132352503+Pedroza1308@users.noreply.github.com>
2025-08-21 22:22:11 +0000

    testando-1

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -1,19 +1,10 @@
-
 export default function Home(){
   return(
     <div> 
       <img src="/assets/foto-minha.jpg" height={300} width={300}></img>
       <h1>Olá, me chamo Pedro Alves</h1>
       <hr></hr>
-      <p>Tenho 21 anos e sou estudante do quinto/sexto período do curso Ciência da
-      computação na Universidade Católica de Pernambuco. Gosto muito de aprender
-      e conhecer novas pessoas. Duas curiosidades sobre minha pessoa é que:
-      Primeiro, antes de ingressar na católica, fiz dois períodos de matemática
-      bacharelado na Universidade Federal de Pernambuco porém não acabei me
-      identificando muito com o curso. Segundo: já fui campeão pernambucano de
-      futsal sub-11 pela escolinha HS society. Sou o caçula de três irmãos e amo
-      muito minha família.
-      </p>
+      <p>Teste1</p>
       <hr></hr>
       <p>Sou apaixonado por música e pra compartilhar um pouco mais de mim vou mostrar uma canção que estive escutando bastante recentemente: </p>
       <audio src="/assets/a-primeira-vista.mp4" controls></audio>
@@ -22,16 +13,3 @@ export default function Home(){
   )
     
 }
-
-//<!DocumentType Html>
-  //<html>
-    //<head>
-
-    //</head>
-    //<body>
-      //<h1></h1> 
-      //<p></p>
-//      <></>
-    //</body>
-  //</html>
-  
