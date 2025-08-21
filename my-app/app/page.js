e24e40c77a49a2797ae4c8eceb7240a56e762622
Pedro_Alves <132352503+Pedroza1308@users.noreply.github.com>
2025-08-21 22:19:14 +0000

    corrigindo-1

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -5,11 +5,15 @@ export default function Home(){
       <img src="/assets/foto-minha.jpg" height={300} width={300}></img>
       <h1>Olá, me chamo Pedro Alves</h1>
       <hr></hr>
-      <p>Tenho 21 anos e sou estudante do quinto/sexto período do curso Ciência da computação na Universidade Católica de Pernambuco.
-      Gosto muito de aprender e conhecer novas pessoas. Duas curiosidades sobre minha pessoa é que: Primeiro, antes de ingressar 
-      na católica, fiz dois períodos de matemática bacharelado na Universidade Federal de Pernambuco porém não acabei me identificando
-      muito com o curso. Segundo: já fui campeão pernambucano de futsal sub-11 pela escolinha HS society. Sou o caçula de três irmãos e
-      amo muito minha família.</p>
+      <p>Tenho 21 anos e sou estudante do quinto/sexto período do curso Ciência da
+      computação na Universidade Católica de Pernambuco. Gosto muito de aprender
+      e conhecer novas pessoas. Duas curiosidades sobre minha pessoa é que:
+      Primeiro, antes de ingressar na católica, fiz dois períodos de matemática
+      bacharelado na Universidade Federal de Pernambuco porém não acabei me
+      identificando muito com o curso. Segundo: já fui campeão pernambucano de
+      futsal sub-11 pela escolinha HS society. Sou o caçula de três irmãos e amo
+      muito minha família.
+      </p>
       <hr></hr>
       <p>Sou apaixonado por música e pra compartilhar um pouco mais de mim vou mostrar uma canção que estive escutando bastante recentemente: </p>
       <audio src="/assets/a-primeira-vista.mp4" controls></audio>
